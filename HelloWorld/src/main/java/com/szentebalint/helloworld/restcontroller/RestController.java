d4790d409c0bfebd8c7f3ff2432a4b55f2f6092c
szentebalint0 <balint.szente@stud.uni-corvinus.hu>
2025-08-13 14:18:51 +0200

    test

--- HelloWorld/src/main/java/com/szentebalint/helloworld/restcontroller/RestController.java
+++ HelloWorld/src/main/java/com/szentebalint/helloworld/restcontroller/RestController.java
@@ -1,4 +1,13 @@
 package com.szentebalint.helloworld.restcontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
 public class RestController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
 }
