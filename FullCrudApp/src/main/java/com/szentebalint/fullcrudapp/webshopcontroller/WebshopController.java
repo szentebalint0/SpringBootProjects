3ff9c093cba966ea84086d600e85776677133284
szentebalint0 <balint.szente@stud.uni-corvinus.hu>
2025-09-10 18:04:45 +0200

    making global exception handler class

--- FullCrudApp/src/main/java/com/szentebalint/fullcrudapp/webshopcontroller/WebshopController.java
+++ FullCrudApp/src/main/java/com/szentebalint/fullcrudapp/webshopcontroller/WebshopController.java
@@ -2,12 +2,8 @@ package com.szentebalint.fullcrudapp.webshopcontroller;
 
 import com.szentebalint.fullcrudapp.dao.ProductDAO;
 import com.szentebalint.fullcrudapp.entity.Product;
-import com.szentebalint.fullcrudapp.webshopcontroller.exceptions.ProductError;
 import com.szentebalint.fullcrudapp.webshopcontroller.exceptions.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,29 +26,6 @@ public class WebshopController {
             throw new ProductNotFoundException("Product not found with id of " + productId);
         }
         return productDAO.getProduct(productId);
-
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ProductError> handleException(ProductNotFoundException exc) {
-
-        ProductError error = new ProductError();
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ProductError> handleAll(Exception exc) {
-
-        ProductError error = new ProductError();
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
 
     }
 
---
+++ FullCrudApp/src/main/java/com/szentebalint/fullcrudapp/webshopcontroller/exceptions/ExceptionHandlerAdvice.java
@@ -0,0 +1,34 @@
+package com.szentebalint.fullcrudapp.webshopcontroller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<ProductError> handleException(ProductNotFoundException exc) {
+
+        ProductError error = new ProductError();
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ProductError> handleException(Exception exc) {
+
+        ProductError error = new ProductError();
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
+    }
+
+}
